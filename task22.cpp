319309c125cc92e6b48d93454c97d22008b17e6e
wyz <warmcongee@gmail.com>
2022-09-28 02:00:02 +0800

    Completed task21

--- task22.cpp
+++ task22.cpp
@@ -6,54 +6,15 @@
 #include <string>
 
 
-bool num_flag[1000000001];
 
-int get_unique_num(int num, int left, bool left_start){
-    bool flag = false;
-    if(left==1){
-        for(int i = 1; i <= num; i++){
-            if(!num_flag[i]){
-                return i;
-            }
-        }
-    } else {
-        if(left%2){
-            left = (left-1)/2;
-        } else {
-            left = left/2;
-        }
-        if(left_start){
-            int i = 1;
-            while (i<=num){
-                if(!num_flag[i]){
-                    flag = !flag;
-                    if(flag){
-                        num_flag[i] = true;
-                    }
-                }
-                i++;
-            }
-        } else {
-            int i = num;
-            while (i >= 1){
-                if(!num_flag[i]){
-                    flag = !flag;
-                    if(flag){
-                        num_flag[i] = true;
-                    }
-                }
-                i--;
-            }
-        }
-        return get_unique_num(num, left, !left_start);
-    }
-
+int get_unique_num(int n) {
+    return n == 1 ? 1 : 2 * (n / 2 + 1 - get_unique_num(n / 2));
 }
 
 int main(){
     int n;
     std::cin>>n;
-    std::cout<<get_unique_num(n, n, true);
+    std::cout<<get_unique_num(n);
 
     return 0;
 }
